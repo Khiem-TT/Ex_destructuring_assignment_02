4f62f930336f86e3eec5a49c73f29b64af536da2
tranthekhiem14112001 <trankhiem99999@gmail.com>
2023-04-03 15:42:26 +0700

    first commit

---
+++ destructuring_assignment_02.js
@@ -0,0 +1,15 @@
+function detectCollision(objects, point) {
+    const object = objects.find(({x, y, width, height}) =>
+        point.x >= x && point.x <= x + width &&
+        point.y >= y && point.y <= y + height
+    );
+    return object;
+}
+
+const myObjects = [
+    {x: 10, y: 20, width: 30, height: 30},
+    {x: -40, y: 20, width: 30, height: 30},
+    {x: 0, y: 0, width: 10, height: 5}
+];
+
+console.log(detectCollision(myObjects, {x: 4, y: 2}));
